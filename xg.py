bf282d150cde638f82735381d2b587530f29e174
Beat Rupp <beatrupp@gmail.com>
2010-01-28 16:42:43 +0100

    fix back...

--- xg.py
+++ xg.py
@@ -189,7 +189,7 @@ def autorelease(func):
     
     @wraps(func)
     def wrapped(*args, **kw):
-        pool = objc.NSAutoreleasePool.alloc().init()
+        pool = Foundation.NSAutoreleasePool.alloc().init()
         try:
             func(*args, **kw)
         finally:
