94dad504ba649369fc3507b7ff09ecc05ac6ea9d
Beat Rupp <beatrupp@gmail.com>
2010-05-31 10:15:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,22 @@ import bdist_mpkg
 setup(name='PyXG',
     version='0.3.0',
     description='A Python interface to Xgrid',
+    license='BSD',
     author='Brian Granger/Barry Wark/Beat Rupp',
     author_email='ellisonbg@gmail.com',
     url='http://launchpad.net/pyxg',
-    py_modules=['xg']
+    py_modules=['xg'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: MacOS X',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: System :: Distributed Computing'
+        ]
     )
